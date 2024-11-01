4d10ba92b9b3ec64aacfbd11ee2c9902dc63b65a
Acorn Studios <80981239+Acorn-Studios@users.noreply.github.com>
2024-11-01 18:10:56 -0500

    Add files via upload

--- scenes/player.gd
+++ scenes/player.gd
@@ -8,9 +8,8 @@ var wheel_deg : int = 0
 var move_dir : Vector3 = Vector3.ZERO
 var wheel_turn_speed : int = 5
 var input_dir : Vector3 = Vector3.ZERO
-var speed: float = 100.0
-var body_size : float = 1.0  # Half the side length of the square robot body
-var wheel_force : float = 10.0  # Force applied by each wheel
+var speed : float = 100.0
+var win : bool = false
 
 
 func _physics_process(delta):
@@ -22,20 +21,24 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	input_dir.x = Input.get_axis("a", "d")
-	input_dir.z = Input.get_axis("w", "s")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.z)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	if not win:
+		input_dir.x = Input.get_axis("a", "d")
+		input_dir.z = Input.get_axis("w", "s")
+		var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.z)).normalized()
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
 	
 	if Input.is_action_just_pressed("r"):
 		get_tree().reload_current_scene()
 	
 	
 	move_and_slide()
+
+func _on_area_3d_body_entered(body):
+	if body.is_in_group("Player"):
+		$"../RichTextLabel".show()
+		win = true
